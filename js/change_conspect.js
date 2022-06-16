9875cfde61c9f688ca14e554de992305652274b0
Alexander <91263783+Twilight-Al-Sparkle@users.noreply.github.com>
2022-06-16 17:31:42 +0300

    Add conspects

---
+++ js/change_conspect.js
@@ -0,0 +1,277 @@
+let allFrame = document.querySelectorAll('#frame');
+let nums = document.querySelectorAll('.assist__for__labs');
+
+// ---------------------------------------------------------
+
+let C1= document.querySelector('#slide1');
+let iframeC1= document.querySelector('.frame1');
+
+C1.addEventListener('click', function () {
+    for (let elem of allFrame) {
+       elem.style.display = 'none';
+    }
+    iframeC1.style.display = "block";
+    console.log('+')
+ })
+
+// ---------------------------------------------------------
+
+let C2 = document.querySelector('#slide2');
+let iframeC2 = document.querySelector('.frame2');
+
+C2.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC2.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C3 = document.querySelector('#slide3');
+let iframeC3 = document.querySelector('.frame3');
+
+C3.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC3.style.display = "block";
+   console.log('+')
+})
+
+
+// ---------------------------------------------------------
+
+let C4 = document.querySelector('#slide4');
+let iframeC4 = document.querySelector('.frame4');
+
+C4.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC4.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C5 = document.querySelector('#slide5');
+let iframeC5 = document.querySelector('.frame5');
+
+C5.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC5.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C6 = document.querySelector('#slide6');
+let iframeC6 = document.querySelector('.frame6');
+
+C6.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC6.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C7 = document.querySelector('#slide7');
+let iframeC7 = document.querySelector('.frame7');
+
+C7.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC7.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C8 = document.querySelector('#slide8');
+let iframeC8 = document.querySelector('.frame8');
+
+C8.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC8.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C9 = document.querySelector('#slide9');
+let iframeC9 = document.querySelector('.frame9');
+
+C9.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC9.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C10 = document.querySelector('#slide10');
+let iframeC10 = document.querySelector('.frame10');
+
+C10.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC10.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C11 = document.querySelector('#slide11');
+let iframeC11 = document.querySelector('.frame11');
+
+C11.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC11.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C12 = document.querySelector('#slide12');
+let iframeC12 = document.querySelector('.frame12');
+
+C12.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC12.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C13 = document.querySelector('#slide13');
+let iframeC13 = document.querySelector('.frame13');
+
+C13.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC13.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C14 = document.querySelector('#slide14');
+let iframeC14 = document.querySelector('.frame14');
+
+C14.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC14.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C15 = document.querySelector('#slide15');
+let iframeC15 = document.querySelector('.frame15');
+
+C15.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC15.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C16 = document.querySelector('#slide16');
+let iframeC16 = document.querySelector('.frame16');
+
+C16.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC16.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C17 = document.querySelector('#slide17');
+let iframeC17 = document.querySelector('.frame17');
+
+C17.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC17.style.display = "block";
+   console.log('+')
+})
+
+
+// ---------------------------------------------------------
+
+let C18 = document.querySelector('#slide18');
+let iframeC18 = document.querySelector('.frame18');
+
+C18.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC18.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C19 = document.querySelector('#slide19');
+let iframeC19 = document.querySelector('.frame19');
+
+C19.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC19.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C20 = document.querySelector('#slide20');
+let iframeC20 = document.querySelector('.frame20');
+
+C20.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC20.style.display = "block";
+   console.log('+')
+})
+
+// ---------------------------------------------------------
+
+let C21 = document.querySelector('#slide21');
+let iframeC21 = document.querySelector('.frame21');
+
+C21.addEventListener('click', function () {
+   for (let elem of allFrame) {
+      elem.style.display = 'none';
+   }
+   iframeC21.style.display = "block";
+   console.log('+')
+})
